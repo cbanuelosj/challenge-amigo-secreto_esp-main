248a9688515720f12aa1896a5e4ca2913604d591
cesarb <cbanuelosj@outlook.com>
2025-02-17 06:38:36 -0600

    Se agrega la funcion actualizarListaAmigos, la cual muestra el nuevo nombre  en la pagina web

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombresAmigos = [];
+const ulListaAmigos = document.getElementById('listaAmigos');
 
 function agregarAmigo(){
-    let cuadroTextoNomAmigo = document.getElementById('amigo');
-    let nombreAmigo = cuadroTextoNomAmigo.value;
+    const cuadroTextoNombreAmigo = document.getElementById('amigo');
+    const nombreAmigo = cuadroTextoNombreAmigo.value;
+
     if( nombreAmigo == ''){
         alert('Por favor escribe el nombre de un amigo');
     }
     else{
-        const listaAmigos = document.getElementById('listaAmigos');
-        listaAmigos.innerHTML += `<li>${nombreAmigo}</li>`;
-        cuadroTextoNomAmigo.value = '';
+        cuadroTextoNombreAmigo.value = '';
+        listaNombresAmigos.push(nombreAmigo);
+        actualizarListaAmigos(nombreAmigo)
     }
+}
+
+function actualizarListaAmigos(nuevoAmigo){
+    ulListaAmigos.innerHTML += `<li>${nuevoAmigo}</li>`;
 }
